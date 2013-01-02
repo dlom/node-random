bdadbdb1cc87fc603c2aa478f579724c310936d8
Mark Old <dlom234@gmail.com>
2013-01-01 20:59:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,125 @@
+# node-random -- A random.org api wrapper
+
+## Install
+
+`npm install node-random`
+
+## Usage
+
+The `options` argument is always optional.
+
+`numbers` is an alias for `integers`
+
+```javascript
+var random = require("node-random");
+
+random.integers(options, function(error, data) {
+    // data is an array of numbers
+});
+
+random.sequences(options, function(error, data) {
+    // data is an array of numbers
+});
+
+random.strings(options, function(error, data) {
+    // data is an array of strings
+});
+
+random.quota(options, function(error, data) {
+    // data is an integer
+});
+```
+
+## Options Defaults
+
+### `random.integers`, `random.numbers`
+
+See [here](http://www.random.org/clients/http/#integers) for the meanings (Option names are slightly different);
+
+* `number` - `1`
+* `minimum` - `0`
+* `maximum` - `10000`
+* `columns` - `1` (When this is more than 1, `data` is a 2D array -- rows by columns)
+* `base` - `10`
+* `random` - `"new"`
+
+### `random.sequences`
+
+See [here](http://www.random.org/clients/http/#sequences) for the meanings (Option names are slightly different);
+
+* `minimum` - `0`
+* `maximum` - `10000`
+* `columns` - `1` (When this is more than 1, `data` is a 2D array -- rows by columns)
+* `base` - `10`
+* `random` - `"new"`
+
+### `random.numbers`
+
+See [here](http://www.random.org/clients/http/#strings) for the meanings (Option names are slightly different);
+
+* `number` - `1`
+* `length` - `10`
+* `digits` - `true`
+* `upper` - `true`
+* `lower` - `true`
+* `unique` - `false`
+* `random` - `"new"`
+
+### `random.quota`
+
+See [here](http://www.random.org/clients/http/#quota) for the meanings (Option names are slightly different);
+
+* `ip` - `""`
+
+## Examples
+
+```javascript
+var random = require("node-random");
+
+// Get 2 random numbers between 1 and 6
+console.log("Rolling 2 dice");
+random.numbers({
+    "number": 2,
+    "minimum": 1,
+    "maximum": 6
+}, function(error, data) {
+    if (error) throw error;
+    data.forEach(function(d) {
+        console.log(d);
+    });
+});
+
+// Get a random 3x3 grid and print the middle vertical row
+random.sequences({
+    "minimum": 1,
+    "maximum": 9,
+    "columns": 3
+}, function(error, data) {
+    if (error) throw error;
+    console.log(data);
+    console.log(data[0][1]);
+    console.log(data[1][1]);
+    console.log(data[2][1]);
+});
+
+// Get 20 different lowercase characters
+random.strings({
+    "length": 1,
+    "number": 20,
+    "upper": false,
+    "digits": false
+}, function(error, data) {
+   if (error) throw error;
+   console.log(data.join(" and "));
+});
+
+// Check your quota
+random.quota(function(error, data) {
+    console.log("Remaining bytes: " + quota)
+});
+```
+
+## Aliases
+
+* `random.numbers` = `random.integers`
+* `random.sequence` = `random.sequences`
---
+++ random.js
@@ -0,0 +1,191 @@
+var request = require("request");
+var qs = require("querystring");
+var statusCodes = require("http").STATUS_CODES;
+
+var random = module.exports = {};
+
+var endpoint = "http://www.random.org/";
+
+// Copied and modified from underscore source
+defaults = function(obj) {
+    [].slice.call(arguments, 1).forEach(function(source) {
+        if (source) {
+            for (var prop in source) {
+                if (obj[prop] == null) obj[prop] = source[prop];
+            }
+        }
+    });
+    return obj;
+};
+
+// Copied and modified from underscore source
+extend = function(obj) {
+    [].slice.call(arguments, 1).forEach(function(source) {
+        if (source) {
+            for (var prop in source) {
+                obj[prop] = source[prop];
+            }
+        }
+    });
+    return obj;
+};
+
+var randomMappings = {
+    "number": "num",
+    "minimum": "min",
+    "maximum": "max",
+    "columns": "col",
+    "random": "rnd",
+    "length": "len",
+    "upper": "upperalpha",
+    "lower": "loweralpha"
+};
+
+var onoffMappings = {
+    "true": "on",
+    "false": "off"
+};
+
+remapKeys = function(obj, mappings) {
+    var remapped = extend({}, obj);
+    for (var prop in mappings) {
+        if (obj[prop] != null) {
+            remapped[mappings[prop]] = obj[prop];
+            delete remapped[prop];
+        }
+    }
+    return remapped;
+};
+
+remapValues = function(obj, mappings) {
+    var remapped = extend({}, obj);
+    for (var prop in remapped) {
+        key = JSON.stringify(obj[prop]);
+        if (mappings.hasOwnProperty(key)) {
+            remapped[prop] = mappings[key];
+        }
+    }
+    return remapped;
+};
+
+var parseNumbers = function(raw, opts) {
+    var data = raw.trim().split("\n").map(function(x) {
+        return x.split("\t");
+    });
+    return (opts.col === 1) ? data.map(function(x) { return x[0]; }) : data;
+};
+
+var parseStrings = function(raw, opts) {
+    return raw.trim().split("\n");
+};
+
+var methods = {
+    "integers": {
+        "defaultOpts": {
+            "number": 1,
+            "minimum": 0,
+            "maximum": 10000,
+            "columns": 1,
+            "base": 10,
+            "random": "new"
+        },
+        "parse": parseNumbers
+    },
+    "numbers": "integers",
+    "sequences": {
+        "defaultOpts": {
+            "minimum": 1,
+            "maximum": 10,
+            "columns": 1,
+            "base": 10,
+            "random": "new"
+        },
+        "parse": parseNumbers
+    },
+    "sequence": "sequences",
+    "strings": {
+        "defaultOpts": {
+            "number": 1,
+            "length": 10,
+            "digits": true,
+            "upper": true,
+            "lower": true,
+            "unique": false,
+            "random": "new"
+        },
+        "parse": parseStrings
+    }
+}
+
+for (var method in methods) {
+    if (typeof methods[method] === "string") {
+        console.log(methods[method]);
+        random[method] = random[methods[method]];
+        console.log(random[method] === random[methods[method]]);
+    } else {
+        random[method] = (function(method) {
+            return function(opts, callback) {
+                if (typeof opts === "function" && callback == null) {
+                    callback = opts;
+                    opts = {};
+                } else if (opts == null) {
+                    opts = {};
+                }
+                opts = defaults(opts, methods[method].defaultOpts);
+                opts = extend(opts, { "format": "plain" });
+                opts = remapKeys(opts, randomMappings);
+                opts = remapValues(opts, onoffMappings);
+
+                var url = endpoint + method + "?" + qs.stringify(opts);
+                request.get(url, function(error, response, body) {
+                    var data;
+                    if (!error && response.statusCode !== 200) {
+                        data = null;
+                        if (response.statusCode === 503) {
+                            errorMessage = "503: " + body.replace(/^Error: /g, "");;
+                        } else {
+                            errorMessage = response.statusCode + ": " + statusCodes[response.statusCode] + " (" + url + ")"
+                        }
+                        error = new Error(errorMessage);
+                    } else {
+                        data = methods[method].parse(body, opts);
+                    }
+                    callback && callback(error, data);
+                });
+            };
+        })(method);
+    }
+}
+
+random.quota = function(opts, callback) {
+    var method = "quota";
+    var defaultOpts = {
+        "ip": ""
+    };
+
+    if (typeof opts === "function" && callback == null) {
+        callback = opts;
+        opts = {};
+    } else if (opts == null) {
+        opts = {};
+    }
+    opts = defaults(opts, defaultOpts);
+    opts = extend(opts, { "format": "plain" });
+
+    var url = endpoint + method + "?" + qs.stringify(opts);
+    request.get(url, function(error, response, body) {
+        var data;
+        if (!error && response.statusCode !== 200) {
+            data = null;
+            if (response.statusCode === 503) {
+                errorMessage = "503: " + body.replace(/^Error: /g, "");;
+            } else {
+                errorMessage = response.statusCode + ": " + statusCodes[response.statusCode] + " (" + url + ")"
+            }
+            error = new Error(errorMessage);
+        } else {
+            data = parseInt(body, 10);
+        }
+        callback && callback(error, data);
+    });
+};
